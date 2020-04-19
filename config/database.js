d299779b10f808c577e4fb13b0b96042c01d594a
lakshmydivyasharma <lakshmydivyasharma@users.noreply.github.com>
2020-04-19 10:28:00 -0400

    updated

---
+++ config/database.js
@@ -0,0 +1,6 @@
+// config/database.js
+module.exports = {
+
+    'url' : 'mongodb+srv://gram:cool@cluster0-d1h5k.mongodb.net/Instagram?retryWrites=true&w=majority'
+// looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
+};
